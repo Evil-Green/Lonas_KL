7760311bfb9603507dccaa8bce803f3f91bb59cc
Javier Sayago <admin@lonasdigital.com>
2013-04-12 06:16:57 +0200

    Corregido dynamic sync control

--- fs/sync.c
+++ fs/sync.c
@@ -170,9 +170,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else {
+	if (!early_suspend_active)
+		return 0;
+	else {
 #endif
 
 	struct address_space *mapping = file->f_mapping;
@@ -198,7 +198,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 out:
 	return ret;
 #ifdef CONFIG_DYNAMIC_FSYNC
-  }
+	}
 #endif
 }
 EXPORT_SYMBOL(vfs_fsync_range);
@@ -233,9 +233,9 @@ static int do_fsync(unsigned int fd, int datasync)
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else
+	if (!early_suspend_active)
+		return 0;
+	else
 #endif
 	return do_fsync(fd, 0);
 }
@@ -243,9 +243,9 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else
+	if (!early_suspend_active)
+		return 0;
+	else
 #endif
 	return do_fsync(fd, 1);
 }
@@ -318,9 +318,9 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else {
+	if (!early_suspend_active)
+		return 0;
+	else {
 #endif
 
 	int ret;
@@ -403,7 +403,7 @@ out_put:
 out:
 	return ret;
 #ifdef CONFIG_DYNAMIC_FSYNC
-  }
+	}
 #endif
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
@@ -422,9 +422,9 @@ SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else
+	if (!early_suspend_active)
+		return 0;
+	else
 #endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
